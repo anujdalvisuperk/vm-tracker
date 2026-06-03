5a7f0ec2f7214b588d818fcb8f373fb27367a76c
anujdalvisuperk <anuj.dalvi@superk.in>
2026-06-03 08:24:00 +0530

    jai mata di 2

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -7,8 +7,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
   // This helps us debug in the Vercel logs if keys are missing
   console.warn("Supabase credentials are missing. Check your Vercel Environment Variables.");
 }
-
-//export const supabase = createClient(
-  //supabaseUrl || 'https://placeholder.supabase.co', 
-  //supabaseAnonKey || 'placeholder'
-//);
+export const supabase = createClient(
+  supabaseUrl || 'https://kfkreohqyvlcdqehhfvg.supabase.co', 
+  supabaseAnonKey || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtma3Jlb2hxeXZsY2RxZWhoZnZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0Mjc4ODcsImV4cCI6MjA5NjAwMzg4N30.Iqle_5JI7jKCrE68zTqud9Gc1Ej0yNsHlUaA7GhIg4A'
+);

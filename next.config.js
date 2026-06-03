114e6402353fa4c5c120b4094a0113587ef6be3b
anujdalvisuperk <anuj.dalvi@superk.in>
2026-06-03 09:57:43 +0530

    config file update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+//const nextConfig = {};
 
-module.exports = nextConfig;
+//module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'kfkreohqyvlcdqehhfvg.supabase.co',
+          port: '',
+          pathname: '/storage/v1/object/public/**',
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
